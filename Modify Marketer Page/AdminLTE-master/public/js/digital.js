a03df439812876637af160368b97f30a0cc8c5c6
velantennison <velantennison1998@gmail.com>
2021-03-24 17:11:05 +0530

    marketer

--- Modify Marketer Page/AdminLTE-master/public/js/digital.js
+++ Modify Marketer Page/AdminLTE-master/public/js/digital.js
@@ -1,10 +1,17 @@
 var ImgUrl;
 var Img1Url;
-var videoUrl;
+var Vid1;
+
 var files = [];
 var files1 = [];
-var files2 = [];
+var files3 = []; 
+var uploadTask1;
+var uploadTask2;
+var uploadTask3;
+var content_,domain_,phone_,mail_,Add_,Shop_address_,zone_,budget_,gst_1_;
 
+var link;
+var cost_add,cost,total_cost;
 
 
 var reader = new FileReader();
@@ -42,64 +49,197 @@ document.getElementById("select1_").onclick = function(e){
   input1.click();
   }
 
-  //video select
   document.getElementById("v_select").onclick = function(e){
     var input2 = document.createElement('input');
     input2.type = 'file';
   input2.onchange = e => {
-    files2 = e.target.files;
+    files3 = e.target.files;
     reader = new FileReader();
     reader.onload = function() {
-        document.getElementById("video_").src = reader.result;
-      
-
+        document.getElementById("video_1").src = reader.result;
+       
     }
-    reader.readAsDataURL(files2[0]);
+    reader.readAsDataURL(files3[0]);
   }
   input2.click();
   }
 
+
   // gst calculation
   function gst1(){
       
-    var cost = document.getElementById("budget").value;
-    var gst_amount = cost * 0.18;
+    
+    var gst_amount = total_cost * 0.18;
     document.getElementById("gst1_").value = gst_amount;
 }
 // display div when click the click here button
-document.getElementById("click_").onclick = function(){
-  alert("welcome");
-  document.getElementById("add_video").style.display = "block";
+// document.getElementById("click_").onclick = function(){
+//   alert("welcome");
+//   document.getElementById("add_video").style.display = "block";
 
-  document.getElementById("video_div").style.display = "block";
-  document.getElementById("zone_div").style.display = "block";
-  document.getElementById("cost_div").style.display = "block";
+//   document.getElementById("video_div").style.display = "block";
+//   document.getElementById("zone_div").style.display = "block";
+//   document.getElementById("cost_div").style.display = "block";
 
 
+// }
+
+// checked or not and payment calculation
+function check_() {
+  var checkBox = document.getElementById("myCheck");
+  var text = document.getElementById("text");
+  if (checkBox.checked == true){
+    alert('Do you want to add Video Ad');
+    document.getElementById("add_video").style.display = "block";
+    document.getElementById("video_div").style.display = "block";
+    document.getElementById("zone_div").style.display = "block";
+    document.getElementById("cost_div").style.display = "block";
+    cost = document.getElementById("budget").value;
+    
+    total_cost = parseFloat(cost_add)+parseFloat(cost);
+   document.getElementById("total_").value = total_cost;
+    console.log(total_cost);
+
+  
+  } else if(checkBox.checked == false) {
+    
+    document.getElementById("add_video").style.display = "none";
+    document.getElementById("video_div").style.display = "none";
+    document.getElementById("zone_div").style.display = "none";
+    document.getElementById("cost_div").style.display = "none";
+    total_cost = parseFloat(total_cost)+parseFloat(cost);
+   document.getElementById("total_").value = total_cost;
+    console.log(total_cost);
+     
+  }
 }
 
+//video ad
+function video_ad(){
+  let L= document.getElementsByName("video");
+  for( i = 0; i < L.length; i++){
+    if(L[i].checked){
+      link = L[i].value;
+      console.log(link);
+    }
+    else{
+    
+  }
+}
+ 
+}
+ // image cost
+ function image_cost(){
+   let C = document.getElementsByName("Image");
+   for(i = 0; i < C.length; i++){
+    if(C[i].checked){
+      cost_add = C[i].value;
+      document.getElementById("cost_").value = cost_add;
+      if(cost_add == 1250){
+        alert("Do you want to add your own Image AD");
+        document.getElementById("image1_div").style.display = "block";
+        document.getElementById("image_div").style.display = "none";
+        document.getElementById("gst1_").value = "216";
+      }else if(cost_add == 1500){
+        alert("Do you want to add Custom Image AD");
+        document.getElementById("image_div").style.display = "block";
+        document.getElementById("image1_div").style.display = "none";
+        document.getElementById("gst1_").value = "225";
+      }
+    }
+    
+}
+   
+ }
+//image radio 
+// document.getElementById("my_banner").onclick = function() {
+//   alert("welcome");
+//  document.getElementById("image1_div").style.display = "block";
+//  document.getElementById("image_div").style.display = "none";
+
+
+// }                   
+// document.getElementById("new_banner").onclick = function() {
+//   alert("hello");
+//  document.getElementById("image_div").style.display = "block";
+//  document.getElementById("image1_div").style.display = "none";
+
+
+
+// }
+
 // add to database digital table
+function dataget(){
+
+  setTimeout(function () { 
+    
+ 
+uploadTask1.snapshot.ref.getDownloadURL().then(function(url1){
+  ImgUrl = url1;
+  console.log("q1" + ImgUrl);
+  uploadTask3.snapshot.ref.getDownloadURL().then(function(url2){
+    Vid1 = url2;
+    console.log("q3" + Vid1);
+
+  uploadTask2.snapshot.ref.getDownloadURL().then(function(url3){
+    Img1Url = url3;
+    console.log("q2" +  Img1Url);
+
+    firebase.database().ref('Digital_marketing/' +  phone_ ).set({
+      Logo:  ImgUrl,
+      ADD:  Img1Url,
+      Content:  content_,
+      Domain: domain_,
+      Mobile_number: phone_,
+      Mail:  mail_,
+      Client_address: Add_,
+      Shop_address:  Shop_address_,
+      Video_url: Vid1,
+     
+      Video_ad: link, 
+    
+      Zone: zone_,
+      Cost: budget_,
+    
+      GST: gst_1_
+     
+    
+    });
+    alert("Data Added Successfully");
+  });
+});
+});
+
+},10000);
+ 
+}
+
 
 document.getElementById("digital_payment").onclick = function() {
 
   // alert("hello darling");
-
-  var content_ = document.getElementById("content_").value;
-  var domain_ = document.getElementById("domain").value;
-  var phone_ = document.getElementById("phonenum_").value;
-  var mail_ = document.getElementById("mail_").value;
-  var Add_ = document.getElementById("address1_").value;
-  var Shop_address_ = document.getElementById("s_address_").value;
-  var zone_ = document.getElementById("zone").value;
-  var budget_ = document.getElementById("budget").value;
-  var gst_1_ = document.getElementById("gst1_").value;
+  
+   content_ = document.getElementById("content_").value;
+   domain_ = document.getElementById("domain").value;
+   phone_ = document.getElementById("phonenum_").value;
+   mail_ = document.getElementById("mail_").value;
+   Add_ = document.getElementById("address1_").value;
+   Shop_address_ = document.getElementById("s_address_").value;
+   zone_ = document.getElementById("zone").value;
+   budget_ = document.getElementById("budget").value;
+   gst_1_ = document.getElementById("gst1_").value;
+ 
 
   console.log(domain_);
+  // console.log(link);
 
-  var uploadTask1 = firebase.storage().ref('Digital_marketing/' +  phone_ +".png.1").put(files[0]);
-  var uploadTask2 = firebase.storage().ref('Digital_marketing/' +  phone_ +".png.2").put(files1[0]);
-  var uploadTask3 = firebase.storage().ref('Digital_marketing/' +  phone_ +".mp4.1").put(files2[0]);
-  uploadTask.on('state_changed', function(snapshot)
+
+   uploadTask1 = firebase.storage().ref('Digital_marketing/' +  phone_ +".png.1").put(files[0]);
+   uploadTask2 = firebase.storage().ref('Digital_marketing/' +  phone_ +".png.2").put(files1[0]);
+   uploadTask3 = firebase.storage().ref('Digital_marketing/' +  phone_ +".mp4.3").put(files3[0]);
+ 
+  link = "null";
+  uploadTask1.on('state_changed', function(snapshot)
   {
     //   var progress = (snapshot.bytesTransfered / snapshot.totalBytes) * 100;
     //   document.getElementById('UpProgress').innerHTML = 'upload'+100+'%';
@@ -110,37 +250,73 @@ document.getElementById("digital_payment").onclick = function() {
   },
 
   function(){
-      uploadTask.snapshot.ref.getDownloadURL().then(function(url1){
-        ImgUrl = url1;
-     
-        uploadTask.snapshot.ref.getDownloadURL().then(function(url2){
-          Img1Url = url2;
+   
+    dataget();
+       
+   
+});
 
-      
-      firebase.database().ref('Projects1/' + month_and_year_ + Client_number_).set({
-                Logo: ImgUrl,
-                Client_name:Client_name_,
-                Design_color: Project_color_,
-                project_number: Project_Number_,
-                Client_mail:Client_mail_,
-                Domain_cost:domain_price,
-                Client_number:Client_number_,
-                shop_address:shop_address_,
-                GST: gst_,
-                Client_address:Client_address_,
-                Project_description:Project_description_,
-                select_domain:select_domain_,
-                month_and_year:month_and_year_,
-                Choosen_plan: Plan_,
-                Plan_price: budget_price,
-                Marketer_mobile:marketer_mob,
-                MARKETER_EMAIL: user_email_fr_lg2
-        });
-      alert("Image Added Successfully");
-  }
-
-);
-},
-);
 
 }
+
+
+// function paymentProcess(){
+//   //     var domainprice = document.getElementById("domain_cost").value;
+//   // var price = document.getElementById("Plan_price").value;
+//   // var gst = document.getElementById("domain_GST").value;
+    
+//   //     total = parseFloat(gst) + parseFloat(price) + parseFloat(domainprice);
+  
+     
+  
+//   var options = {
+//       "key": "rzp_test_GmJyzKJA6xsOC6", // Enter the Key ID generated from the Dashboard
+//       "amount": total * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 means 50000 paise or ₹500.
+//       "currency": "INR",
+//       "name": "Hyper Mart",
+//       "description": "Tutorial",
+//       // "callback_url": 'https://www.google.com',
+//       "image": "logo.png",// Replace this with the order_id created using Orders API (https://razorpay.com/docs/api/orders).
+//       "handler": function (response){
+//           savetoDB(response);
+//           $('#myModal').modal();
+//       },
+//       "prefill": {
+//           "name": "",
+//           "email": "",
+//           "contact": "",
+//           "phoneno" : ""
+//       },
+//       "notes": {
+//           "address": "note value"
+//       },
+//       "theme": {
+//           "color": "blue"
+//       }
+//   }
+//   var propay = new Razorpay(options);
+//   propay.open();
+//       }   
+//       function savetoDB(response){
+//         {
+//         console.log(response)
+//        datainsert();
+//         var name = document.getElementById('fname').value;
+//         var payRef = firebase.database().ref('payment/');
+       
+//         if (typeof response.razorpay_payment_id == 'undefined' || response.razorpay_payment_id < 1) {
+//             // redirect_url = 'preloadergeeks.html';
+//             console.log('failed');
+//            } else { 
+//             //    redirect_url = 'preloadergeeks.html';
+//                console.log('success');
+//                payRef.child(name).set({
+//                 NameOfStudent: name,
+//             payment_id : response.razorpay_payment_id
+//             })
+        
+//            }
+//     }
+    
+//     //    location.href = redirect_url;
+//     }
